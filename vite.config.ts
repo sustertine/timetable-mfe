06d64a26f3ecf875141f7de02dff69cef53a2e6f
Tine Šuster <tine.suster@gmail.com>
2024-04-16 09:50:21 +0200

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import VueDevTools from 'vite-plugin-vue-devtools'
+
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+import * as path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
+  plugins: [
+    vue(),
+    vueJsx(),
+    VueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      // '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": path.resolve(__dirname, "./src"),
+    }
+  }
+})
